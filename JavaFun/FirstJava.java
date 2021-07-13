15050ad35679b47433c9db75f18140d9679c9001
lhm-david <81556571+lhm-david@users.noreply.github.com>
2021-07-12 18:26:01 -0700

    Add files via upload

---
+++ JavaFun/FirstJava.java
@@ -0,0 +1,7 @@
+public class FirstJava {
+    public static void main(String[] args) {
+        System.out.println("My name is David");
+        System.out.println("I am 27 years old");
+        System.out.println("My hometown is China");
+    }
+}
