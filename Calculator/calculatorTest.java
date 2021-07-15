1156bad5c226f0b937ec2bfe5c8ae0a3489cafbc
lhm-david <81556571+lhm-david@users.noreply.github.com>
2021-07-15 14:44:18 -0700

    Delete Calculator directory

--- Calculator/calculatorTest.java
+++
@@ -1,7 +0,0 @@
-public class calculatorTest {
-    public static void main(String[] args){
-        calculator operation = new calculator(10.5, 5.2);
-        operation.performOperation();
-        operation.getResults();
-    }
-}
