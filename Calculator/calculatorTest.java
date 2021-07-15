abd14be527ba88a96b5c14e7715c4ab6263bbf42
lhm-david <81556571+lhm-david@users.noreply.github.com>
2021-07-15 14:31:19 -0700

    Add files via upload

---
+++ Calculator/calculatorTest.java
@@ -0,0 +1,7 @@
+public class calculatorTest {
+    public static void main(String[] args){
+        calculator operation = new calculator(10.5, 5.2);
+        operation.performOperation();
+        operation.getResults();
+    }
+}
