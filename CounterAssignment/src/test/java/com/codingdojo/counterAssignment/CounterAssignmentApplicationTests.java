d99d886db5269749c5e33007cc6452694c9797bb
lhm-david <81556571+lhm-david@users.noreply.github.com>
2021-07-21 18:28:40 -0700

    Add files via upload

---
+++ CounterAssignment/src/test/java/com/codingdojo/counterAssignment/CounterAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.counterAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CounterAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
